d111503619032caa3835885bbd8fae3ae9737f99
Carolyn <yintingchien@gmail.com>
2022-09-28 11:39:16 -0400

    fixed aos

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,14 +1,20 @@
 import styles from '../../styles/Project.module.css'
 import projects from '../../assets/projects'
 import ProjectBox from '../Projecctbox/ProjectBox'
+import Aos from 'aos'
+import { useEffect } from 'react'
 
+import 'aos/dist/aos.css'
 export default function Projects() {
-  console.log(projects)
+  useEffect(() => {
+    Aos.init({ duration: 2500 })
+  }, [])
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.projectscontainer}>
         {projects.map((project, index) => (
-          <ProjectBox project={project} key={index} />
+          <ProjectBox project={project} key={index} data-aos="flip-up" />
         ))}
       </div>
     </div>
